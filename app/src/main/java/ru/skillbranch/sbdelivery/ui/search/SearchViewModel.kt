8d80695f8f43e9fa055e1054f8ab48acc7d17109
th1ngshappen <th1ngshappen@yandex.ru>
2021-12-29 21:43:23 +0700

    fix for a test

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/search/SearchViewModel.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/search/SearchViewModel.kt
@@ -1,19 +1,13 @@
 package ru.skillbranch.sbdelivery.ui.search
 
-import android.os.Handler
-import androidx.core.os.postDelayed
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
 import ru.skillbranch.sbdelivery.core.BaseViewModel
 import ru.skillbranch.sbdelivery.domain.SearchUseCase
-import ru.skillbranch.sbdelivery.repository.error.AnotherError
 import ru.skillbranch.sbdelivery.repository.error.EmptyDishesError
 import ru.skillbranch.sbdelivery.repository.mapper.DishesMapper
-import ru.skillbranch.sbdelivery.ui.main.MainState
 import java.util.concurrent.TimeUnit
 
 class SearchViewModel(
@@ -59,7 +53,7 @@ class SearchViewModel(
             }, {
                 if (it is EmptyDishesError) {
                     action.value = SearchState.Error(it.messageDishes)
-                    setSearchEvent(searchEvent)
+                    setSearchEvent(searchEvent.skip(1))
                 } else {
                     action.value = SearchState.Error("to pass tests")
                 }
